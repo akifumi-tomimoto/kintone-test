4aca5398fd787cdd16fb394770107f7b49db2643
Akifumi Tomimoto <akihumi.tomimoto@asnet.co.jp>
2024-07-01 15:20:56 +0900

    update

--- src/js/test.js
+++ src/js/test.js
@@ -5,7 +5,7 @@
       const loginUser = kintone.getLoginUser().code;
   
       // ログインユーザーに応じた処理をここに記述
-      kintone.app.getHeaderSpaceElement().textContent = `こんにちは！${loginUser}はログインしました`;
+      kintone.app.getHeaderSpaceElement().textContent = `こんにちは！${loginUser}はログインしました!githubへのpushで更新確認`;
       return event;
     });
   })();
