195d7dc3260c67ef513df0cf7a8673b4105f03a7
Akifumi Tomimoto <akihumi.tomimoto@asnet.co.jp>
2024-07-02 17:56:01 +0900

    update

--- uploader.js
+++ uploader.js
@@ -1,7 +1,10 @@
 import {execSync} from 'child_process';
 import {glob} from 'glob';
 
-const command = `npx kintone-customize-uploader --base-url $npm_package_config_baseurl --username $npm_config_username --password $npm_config_password `;
+console.log(process.argv[4])
+console.log(process.argv[5])
+console.log(process.argv[6])
+const command = `npx kintone-customize-uploader --base-url ${process.argv[4]} --username ${process.argv[5]} --password ${process.argv[6]} `;
 const entries =
   process.argv.slice(2).length > 0
     ? process.argv.slice(2)
