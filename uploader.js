1916c680b159a1536741e9c890835f73b1c762e9
Akifumi Tomimoto <akihumi.tomimoto@asnet.co.jp>
2024-07-02 18:17:25 +0900

    update

--- uploader.js
+++ uploader.js
@@ -1,14 +1,9 @@
 import {execSync} from 'child_process';
 import {glob} from 'glob';
 
-console.log(process.argv[4])
-console.log(process.argv[5])
-console.log(process.argv[6])
-const command = `npx kintone-customize-uploader --base-url ${process.argv[4]} --username ${process.argv[5]} --password ${process.argv[6]} `;
-const entries =
-  process.argv.slice(2).length > 0
-    ? process.argv.slice(2)
-    : glob.sync('src/apps/**/customize-manifest.json');
+
+const command = `npx kintone-customize-uploader --base-url ${process.argv[3]} --username ${process.argv[4]} --password ${process.argv[5]} `;
+const entries = glob.sync('src/apps/**/customize-manifest.json');
 entries.forEach(file => {
   console.log('\nuploading... ', file);
   const result = execSync(command + file);
