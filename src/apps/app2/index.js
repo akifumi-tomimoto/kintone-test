48a5366041f8bf7ae9f21939f56acf2437225688
Akifumi Tomimoto <akihumi.tomimoto@asnet.co.jp>
2024-07-03 10:03:27 +0900

    try GitHub Copilot

--- src/apps/app2/index.js
+++ src/apps/app2/index.js
@@ -1,4 +1,13 @@
 const events = ['app.record.create.show', 'app.record.edit.show'];
 kintone.events.on(events, event => {
-  // ここに処理内容を書く
+  // Hello Worldを表示する
+  const el = kintone.app.record.getHeaderMenuSpaceElement();
+  const hello = 'hello';
+  const world = 'world!';
+  const message = {
+    hello,
+    world
+  };
+  const copied = Object.assign({}, message);
+  el.innerHTML = `<div>${copied.hello} ${copied.world}!</div>`;
 });
