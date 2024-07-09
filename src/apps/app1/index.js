7cfa0f33e6d0e1cbbb7717401178f8038e458808
Akifumi Tomimoto <akihumi.tomimoto@asnet.co.jp>
2024-07-09 09:51:47 +0900

    update

--- src/apps/app1/index.js
+++ src/apps/app1/index.js
@@ -1,11 +1,10 @@
-import userEnv from 'userEnv';
 
 (() => {
   'use strict';
   kintone.events.on('app.record.index.show', (event) => {
     // ログインユーザーのコードを取得
     const loginUser = kintone.getLoginUser().code;
-    const app1ID = userEnv.appIds.app1
+    const app1ID = import.meta.env.VITE_APP1;
 
     // ログインユーザーに応じた処理をここに記述
     kintone.app.getHeaderSpaceElement().textContent = `こんにちは！${loginUser}はログインしました!app1IDは${app1ID}です!`;
---
+++ vite.config.js
@@ -0,0 +1,47 @@
+// vite.config.js
+import { resolve, dirname } from 'path'
+import { defineConfig } from 'vite'
+
+const glob = require('glob');
+
+const basePath = resolve('src', 'apps');
+const environment = process.env.NODE_ENV || 'dev';
+
+// basePath配下の各ディレクトリを複数のentryとする
+const entries = glob.sync('**/index.+(js|ts|tsx)', {cwd: basePath}).reduce(
+  (prev, file) => ({
+    ...prev,
+    [dirname(file)]: resolve(basePath, file),
+  }),
+  {}
+);
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      // エイリアスのキーを正規表現リテラルで指定し、対応するパスを動的に解決
+      '^/userEnv$': resolve(__dirname, `kintone-env/${environment}_env_var.js`),
+    },
+  },
+  build: {
+    lib: {
+      // 複数のエントリーポイントのディクショナリや配列にもできます
+      entry: entries,
+      name: 'MyLib',
+      // 適切な拡張子が追加されます
+      fileName: '[name]'
+    },
+    rollupOptions: {
+      // ライブラリーにバンドルされるべきではない依存関係を
+      // 外部化するようにします
+      external: ['vue'],
+      output: {
+        // 外部化された依存関係のために UMD のビルドで使用する
+        // グローバル変数を提供します
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
